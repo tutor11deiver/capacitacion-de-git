907e23e1dbf5c7d59979ae152abdb1269737cc96
eduardo599 <eduardo599@gmail.com>
2021-09-30 09:14:33 -0500

    Consultas

---
+++ Consultas 2 Sql.sql
@@ -0,0 +1,3 @@
+select * FROM matricula inner join estudiante on  matricula.idEstudiante = estudiante.idAlumnos;
+INSERT INTO tablasimatsql.estudiante (idUsuariosAlumnos, idSede, estrato, idAcudiente) VALUES (5, 1, "2", 5) ;
+INSERT INTO tablasimatsql.profesores (idUsuariosProfesores, usuario, clave) VALUES (4, "Johan55", "JO1234") ;
